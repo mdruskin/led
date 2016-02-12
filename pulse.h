2c16c793b3ab3710ff524ecd8821d80c6f2c2634
mdruskin <mdruskin@Lrrr.local>
2016-02-11 22:51:58 -0800

    Updating pulse to pulse out from middles

--- pulse.h
+++ pulse.h
@@ -14,33 +14,33 @@ namespace pulse {
   int pulseMovePeriod = 2;
   bool up = true;
   byte basePulseHue = 0;
-  byte pulseHue[4];
+  byte pulseHue;
   byte backgroundHue = 160;
   long lastBackgroundMillis = 0;
   int backgroundPeriod = 4000;
   byte backgroundHeat = 128;
+  int pulseCenters[] = {59, 176, 298, 414};
 
   void draw(CRGB leds[NUM_LEDS]) {
     if (periodToggle(lastPulseMillis, pulsePeriod)) {
       // generate new pulse length and hue
-      pulseLength = random16(50, 80);
+      pulseLength = random16(30, 40);
       currentLed = 0;
-      for (int i = 0; i < 4; i++) {
-        pulseHue[i] = random8(basePulseHue, basePulseHue + 16);        
-      }
+      pulseHue = random8(basePulseHue, basePulseHue + 24);
       up = true;
     }
 
     // if we reached the top, go down for all 4
-    if (currentLed >= pulseLength)
+    if (currentLed >= pulseLength) {
       up = false;
+    }
 
     // if we are at the bottom, just fill backfround
     if (!up && currentLed <= 0) {
       fill_solid(leds, NUM_LEDS, CHSV(backgroundHue, 255, backgroundHeat));
       return;
     }
-
+    
     if (periodToggle(lastPulseMoveMillis, pulseMovePeriod)) {
       // background
       for (int i = 0; i < NUM_LEDS; i++) {
@@ -51,30 +51,29 @@ namespace pulse {
       for (int i = 0; i < currentLed; i++) {
         byte value = 128 - i * (128 / pulseLength);
         if (up) {            
-          leds[i] = CHSV(pulseHue[0], 255, value);
-          leds[240 - i] = CHSV(pulseHue[1], 255, value);
-          leds[240 + i] = CHSV(pulseHue[2], 255, value);
-          leds[480 - i] = CHSV(pulseHue[3], 255, value);
+          for (int p = 0; p < 4; p++) {
+            leds[pulseCenters[p] + i] = CHSV(pulseHue, 255, value);
+            leds[pulseCenters[p] - i] = CHSV(pulseHue, 255, value);
+          }
         } else {
-          leds[i] = CHSV(pulseHue[0], 255, value / 4);
-          leds[240 - i] = CHSV(pulseHue[1], 255, value / 4);
-          leds[240 + i] = CHSV(pulseHue[2], 255, value / 4);
-          leds[480 - i] = CHSV(pulseHue[3], 255, value / 4);
+          for (int p = 0; p < 4; p++) {
+            leds[pulseCenters[p] + i] = CHSV(pulseHue, 255, value * 0.8);
+            leds[pulseCenters[p] - i] = CHSV(pulseHue, 255, value * 0.8);
+          }
+
         }
       }
 
       // heads of the pulses
-      leds[currentLed] = CHSV(pulseHue[0], 220, 255);
-      leds[currentLed + 1] = CHSV(pulseHue[0], 220, 255);
-      leds[240 - currentLed] = CHSV(pulseHue[1], 220, 255);
-      leds[240 - currentLed - 1] = CHSV(pulseHue[1], 220, 255);
-      leds[240 + currentLed] = CHSV(pulseHue[2], 220, 255);
-      leds[240 + currentLed + 1] = CHSV(pulseHue[2], 220, 255);
-      leds[480 - currentLed] = CHSV(pulseHue[3], 220, 255);
-      leds[480 - currentLed - 1] = CHSV(pulseHue[3], 220, 255);
+      for (int p = 0; p < 4; p++) {
+        leds[pulseCenters[p] + currentLed] = CHSV(pulseHue, 220, 255);
+        leds[pulseCenters[p] + currentLed + 1] = CHSV(pulseHue, 220, 255);
+        leds[pulseCenters[p] - currentLed] = CHSV(pulseHue, 220, 255);
+        leds[pulseCenters[p] - currentLed - 1] = CHSV(pulseHue, 220, 255);
+      }
 
       if (up)
-        currentLed += 5;
+        currentLed += 3;
       else
         currentLed -= 2;
     }
